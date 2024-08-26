777210aa3397fe5a05c4033a2a429cadd288e9f3
geonwoo1098 <geonwoo1098@gmail.com>
2024-08-26 17:37:04 +0900

    헤더 데이터 작업

---
+++ src/data/header.js
@@ -0,0 +1,108 @@
+import { CiAlarmOn } from "react-icons/ci";
+import { AiFillGithub } from "react-icons/ai";
+import { AiOutlineCodepen } from "react-icons/ai";
+import { AiFillYoutube } from "react-icons/ai";
+
+export const headerMenus = [
+    {
+        title:"홈 페이지",
+        icon:<CiAlarmOn />,
+        src:"/"
+    },
+    {
+        title:"추천 영상",
+        icon:<CiAlarmOn />,
+        src:"/today"
+    },
+    {
+        title:"개발자",
+        icon:<CiAlarmOn />,
+        src:"/developer"
+    },
+    {
+        title:"웹디",
+        icon:<CiAlarmOn />,
+        src:"/webd"
+    },
+    {
+        title:"웹 표준 사이트",
+        icon:<CiAlarmOn />,
+        src:"/website"
+    },
+    {
+        title:"홈 페이지",
+        icon:<CiAlarmOn />,
+        src:"/gsap"
+    },
+    {
+        title:"포폴사이트",
+        icon:<CiAlarmOn />,
+        src:"/port"
+    },
+    {
+        title:"유튜브 클론 사이트",
+        icon:<CiAlarmOn />,
+        src:"/youtube"
+    },
+];
+
+export const searchKeyword = [
+    {
+        title:"utubesite",
+        src:"/search/utubesite"
+    },
+    {
+        title:"HTML",
+        src:"/search/html"
+    },
+    {
+        title:"CSS",
+        src:"/search/css"
+    },
+    {
+        title:"Javascript",
+        src:"/search/javascript"
+    },
+    {
+        title:"React.js",
+        src:"/search/react.js"
+    },
+    {
+        title:"Next.js",
+        src:"/search/next.js"
+    },
+    {
+        title:"Node.js",
+        src:"/search/node.js"
+    },
+    {
+        title:"SQL",
+        src:"/search/sql"
+    },
+    {
+        title:"portfolio",
+        src:"/search/React Portfolio"
+    },
+    {
+        title:"music",
+        src:"/search/japanMusic"
+    },
+];
+
+export const snsLink = [
+    {
+        title:"github",
+        url:"https://github.com/geon1098/utube-site",
+        icon:<AiFillGithub />
+    },
+    {
+        title:"youtube",
+        url:"https://www.youtube.com/",
+        icon:<AiFillYoutube />
+    },
+    {
+        title:"youtube",
+        url:"https://codepen.io/",
+        icon:<AiOutlineCodepen />
+    },
+];
